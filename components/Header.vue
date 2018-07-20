53682c43791b1c0438d1c6824b8f8b71eaa34f54
StormSpirit22 <icestormspirit@gmail.com>
2018-07-20 17:32:25 +0800

    update

---
+++ components/Header.vue
@@ -0,0 +1,104 @@
+<template>
+<div>
+  <div class="line"></div>
+  <div>
+  <el-menu
+    :default-active="activeIndex2"
+    class="el-menu-demo nav"
+    mode="horizontal"
+    background-color="#00C8FF"
+    @select="handleSelect"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    >
+        <img src="~/assets/atn.png" class="image" >
+        <div class="col1">
+            <el-menu-item index="2" class="font">Home</el-menu-item>
+            <el-menu-item index="3" class="font">Blocks</el-menu-item>
+            <el-menu-item index="4" class="font">Transactions</el-menu-item>
+            <el-menu-item index="5" class="font">Accounts</el-menu-item>
+            <el-menu-item index="6" class="font">Dbots</el-menu-item>
+        </div>
+        
+        <el-input v-model="input" class="input" placeholder="Search Address / Tx / Block / Dbot"></el-input>
+        <el-button icon="el-icon-search" class="button">Search</el-button>
+       
+   
+  </el-menu>
+
+</div>
+</div>
+</template>
+
+<style>
+
+    .nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .button {
+        background: #00C8FF;
+        color: #FFF;
+        height: 75%;
+        float: left;
+        margin-left: -90px;
+    }
+
+    .input {
+        width: 290px;
+    }
+
+    .search {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    .col1 {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        left: -10px;
+    }
+
+    .font {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 15px;
+        font-family: Helvetica, Tahoma, Arial;
+
+    }
+
+    .nav {
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 20px;
+    }
+
+    .image {
+        text-align: center;
+        height: 45px;
+    }
+
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1',
+        input: ''
+
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,138 @@
+<template>
+<div>
+    <Header/>
+  <el-main>
+    <el-header class="header" height="240px">
+    <el-row :justify=space-around height="100%" width="100%">
+      <el-col :span="6" >
+        <div class="grid-content" >
+          <img src="~/assets/blocks.png" class="icon">
+            <span >
+              BLOCK
+            </span>
+            <br/>
+            <span >
+              #123456
+            </span>
+        </div>
+        
+      </el-col>
+      
+    </el-row>
+        
+    </el-header>
+    <el-header height="360px" background-color="#FFF">
+
+    </el-header>
+  </el-main>
+  <el-footer>Footer</el-footer>
+</div>
+</template>
+
+<style>
+
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .grid-content {
+    border-radius: 4px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-family:  "Helvetica Neue",Helvetica;
+    color: #757589;
+    font-size: 25px;
+  }
+
+  .block {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .icon {
+    width: 100px;
+    height: 100px;
+  }
+
+  .card {
+    padding: 0px;
+    width: 280px;
+    height: 180px;
+  }
+
+ .header{
+   background-color: #F7F7F9;
+   text-align: center;
+   display: flex;
+   justify-content: space-around;
+   align-items: center;
+  } 
+ .el-footer {
+    background-color: #00C8FF;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #FFF;
+    color: #333;
+    text-align: center;
+    line-height: 560px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
+
+<script>
+import Header from '~/components/Header.vue'
+
+
+  export default {
+    components: {
+      Header
+    },
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1',
+        input: ''
+
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
