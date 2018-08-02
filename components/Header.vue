c4630b53de90dc1dbfce280242dbf7874aceef49
junjia.wan <new_start@live.cn>
2018-08-02 14:34:02 +0800

    menu update

--- components/Header.vue
+++ components/Header.vue
@@ -1,103 +1,141 @@
 <template>
     <div style="width: 100%">
-    <el-menu
-        :default-active="activeIndex2"
-        class="el-menu-demo nav"
-        mode="horizontal"
-        background-color="#00C8FF"
-        @select="handleSelect"
-        text-color="#fff"
-        active-text-color="#ffd04b"
-        >
-        <img src="~/assets/atn.png" class="image" >
-            <div class="left-nav">
-                <el-menu-item index="2" class="font"><nuxt-link to="/">Home</nuxt-link></el-menu-item>
-                <el-menu-item index="3" class="font"><nuxt-link to="/blocks">Blocks</nuxt-link></el-menu-item>
-                <el-menu-item index="4" class="font"><nuxt-link to="/transactions">Transactions</nuxt-link></el-menu-item>
-                <el-menu-item index="5" class="font"><nuxt-link to="/accounts">Accounts</nuxt-link></el-menu-item>
-                <el-menu-item index="6" class="font"><nuxt-link to="/dbots">Dbots</nuxt-link></el-menu-item>
-            </div>
-            <div class="right-nav">
-                <el-input v-model="input" class="input" placeholder="Search Address / Tx / Block / Dbot"></el-input>
-                <el-button icon="el-icon-search" class="button" v-on:click="this.search">Search</el-button>
-            </div>
-    
-    </el-menu>
-
-</div>
+        <el-menu
+            :default-active="activeIndex2"
+            class="el-menu-demo nav"
+            mode="horizontal"
+            background-color="#00C8FF"
+            @select="handleSelect"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            >
+            <img src="~/assets/atn.png" class="image" >
+                <div class="left-nav">
+                    <el-menu-item index="2" class="font"><nuxt-link to="/">Home</nuxt-link></el-menu-item>
+                    <el-menu-item index="3" class="font"><nuxt-link to="/blocks">Blocks</nuxt-link></el-menu-item>
+                    <el-menu-item index="4" class="font"><nuxt-link to="/transactions">Transactions</nuxt-link></el-menu-item>
+                    <el-menu-item index="5" class="font"><nuxt-link to="/accounts">Accounts</nuxt-link></el-menu-item>
+                    <el-menu-item index="6" class="font"><nuxt-link to="/dbots">Dbots</nuxt-link></el-menu-item>
+                </div>
+                <div class="right-nav">
+                    <el-input v-model="input" class="input" placeholder="Search Address / Tx / Block / Dbot"></el-input>
+                    <el-button icon="el-icon-search" class="button" v-on:click="this.search">Search</el-button>
+                </div>
+        
+        </el-menu>
+        <div class="mobile-menu">
+            <img src="~/assets/atn.png" class="image" >
+             <el-dropdown trigger="click">
+                <span class="el-dropdown-link">
+                    <i class="el-icon-menu"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>Home</el-dropdown-item>
+                    <el-dropdown-item>Blocks</el-dropdown-item>
+                    <el-dropdown-item>Transactions</el-dropdown-item>
+                    <el-dropdown-item>Accounts</el-dropdown-item>
+                    <el-dropdown-item>Dbots</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+            <!-- <i class="el-icon-menu"></i> -->
+        </div>
+    </div>
 </template>
 
 <style scoped lang="less">
+.left-nav {
+  width: 30%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  /* margin-left: -10%; */
+}
 
-    .left-nav {
-        width: 30%;
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        align-items: flex-start;
-        /* margin-left: -10%; */
+.right-nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  background: #00c8ff;
+  color: #fff;
+  height: 75%;
+  float: left;
+}
+
+.input {
+  width: 290px;
+}
+
+.search {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.col1 {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.font {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  font-family: Helvetica, Tahoma, Arial;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+}
+
+.nav {
+  color: #ffffff;
+  text-align: center;
+  font-size: 20px;
+}
+
+.image {
+  text-align: center;
+  height: 45px;
+}
+
+.mobile-menu {
+  display: none;
+}
+
+@media screen and (max-width: 991px) {
+  .nav {
+    display: none;
+  }
+
+  .mobile-menu {
+    width: 100%;
+    height: 56px;
+    padding: 3px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: #00c8ff;
+
+    .el-icon-menu {
+      font-size: 32px;
+      color: #ffffff;
     }
-
-    .right-nav {
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        align-items: center;
-    }
-
-    .nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .button {
-        background: #00C8FF;
-        color: #FFF;
-        height: 75%;
-        float: left;
-    }
-
-    .input {
-        width: 290px;
-    }
-
-    .search {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
-
-    .col1 {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-    }
-
-    .font {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 15px;
-        font-family: Helvetica, Tahoma, Arial;
-    }
-
-    a {
-        text-decoration: none;
-        color: #fff;
-    }
-
-    .nav {
-        color: #FFFFFF;
-        text-align: center;
-        font-size: 20px;
-    }
-
-    .image {
-        text-align: center;
-        height: 45px;
-    }
-
+  }
+}
 </style>
 
 <script>
