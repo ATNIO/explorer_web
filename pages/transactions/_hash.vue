a52961a5d0d797df75113c9d7f07be8078700a13
StormSpirit22 <icestormspirit@gmail.com>
2018-08-07 16:12:17 +0800

    update tx details

--- pages/transactions/_hash.vue
+++ pages/transactions/_hash.vue
@@ -435,7 +435,6 @@
             align-items: center;
             justify-content: center;
             width: 100%;
-            height: 100%;
         }
         .textarea {
             width: 350px;
